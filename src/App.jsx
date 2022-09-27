1eeb2dd277c9ff055d67d02d32b774b73d8f81a8
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2022-09-27 00:28:00 -0300

    Criado rotas para paginas

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './Components/Header/Header';
-import { GlobalSyle } from './styles/global'
-
+import MenuBar from './Components/MenuBar/MenuBar';
+import { GlobalStyle, ContainerPages } from './styles/global'
+import Home from './Pages/Home/Home'
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <GlobalSyle />
-    </>
+      <ContainerPages>
+        <MenuBar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </ContainerPages>
+      <GlobalStyle />
+    </BrowserRouter>
   );
 }
 
