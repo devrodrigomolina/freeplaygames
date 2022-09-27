022d2987c4aa201f66c704f68aa1a25189ba8a03
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2022-09-27 00:27:37 -0300

    Criado componente header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
-import { HeaderContainer } from './style'
-import Input from '../Input/Input'
+import React from "react";
+import { HeaderContainer, ContainerInfosUser } from "./style";
+import Input from "../Input/Input";
+import Logo from "../Logo/Logo";
+import CartAndNotify from "../UserSection/CartAndNotify/CartAndNotify";
+import UserAccount from "../UserSection/UserAccount/UserAccount";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <>
       <HeaderContainer>
-        <Input />
+        <Link to='/'>
+          <Logo />
+        </Link>
+        <ContainerInfosUser>
+          <Input />
+          <CartAndNotify />
+          <UserAccount />
+        </ContainerInfosUser>
       </HeaderContainer>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
