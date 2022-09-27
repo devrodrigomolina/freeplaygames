b679d14b0d73110562906e75ea6ff02a6f0a526a
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2022-09-27 00:27:48 -0300

    Inicio da criação do carrossel

---
+++ src/Components/Carrossel/Carrossel.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { CarrosselContainer } from './style'
+
+const Carrossel = () => {
+  return (
+    <CarrosselContainer>
+   
+    </CarrosselContainer>
+  )
+}
+
+export default Carrossel
---
+++ src/Components/Carrossel/style.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const CarrosselContainer = styled.div`
+  width: 100vw;
+  max-width: 95vw;
+  height: 500px;
+  margin: 10px auto;
+  border: 1px solid #78828B;
+  border-radius: 5px;
+  background-image: url('https://cdn.wallpapersafari.com/43/48/lXHd4x.jpg');
+  background-size: cover;
+  background-repeat: no-repeat;
+`
