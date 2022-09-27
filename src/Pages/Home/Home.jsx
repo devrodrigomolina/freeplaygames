b76bae1ee020c33fd5d154c573624df84e062e23
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2022-09-27 00:26:22 -0300

    Criado pagina home

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Carrossel from '../../Components/Carrossel/Carrossel'
+import { HomeContainer } from './style'
+
+const Home = () => {
+  return (
+    <HomeContainer>
+      <Carrossel />
+    </HomeContainer>
+  )
+}
+
+export default Home
---
+++ src/Pages/Home/style.jsx
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const HomeContainer = styled.div`
+  width: calc(100vw - 70px);
+`
