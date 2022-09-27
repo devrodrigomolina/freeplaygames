765fcc7a24b0f086699437da80d95dccb3089623
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2022-09-27 00:27:26 -0300

    Criado componente header

--- src/Components/Header/style.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const HeaderContainer = styled.div`
-  width: 100vw;
-  height: 70px;
-  background-color: #353940;
-`
---
+++ src/Components/Header/style.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.div`
+  width: 100vw;
+  padding-right: 20px;
+  height: 80px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #353940;
+  border-bottom: 1px solid #78828B;
+`
+export const ContainerInfosUser = styled(HeaderContainer)`
+  width: 770px;
+  border-bottom: 1px solid #78828B;
+`
