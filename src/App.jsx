dac91d52edfbe4f90d6d86c3de5a423e3dc8f4ea
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2022-09-27 23:47:38 -0300

    Consumindo dados da api

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,36 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './Components/Header/Header';
 import MenuBar from './Components/MenuBar/MenuBar';
-import { GlobalStyle, ContainerPages } from './styles/global'
+import { GlobalStyle, ContainerPages, MainMiddle } from './styles/global'
 import Home from './Pages/Home/Home'
+import api from './api/api'
+import { useEffect, useState } from 'react';
+import CardsGames from './Components/CardsGames/CardsGames';
+
 
 function App() {
+  const [ data, setData ] = useState([])
+
+  const getGames = async () => {
+    const { data: json } = await api.get(`/games?${'sort-by=release-date'}`)
+    setData(json)
+  }
+
+  useEffect(() => {
+    getGames()
+  }, [])
+
   return (
     <BrowserRouter>
       <Header />
       <ContainerPages>
         <MenuBar />
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-        </Routes>
+        <MainMiddle>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+          </Routes>
+          <CardsGames data={data} />
+        </MainMiddle>
       </ContainerPages>
       <GlobalStyle />
     </BrowserRouter>
