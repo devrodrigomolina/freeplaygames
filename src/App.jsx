b3ba3e0f54ac56bf5422ea0a0d4cd92f324b3812
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2022-10-06 00:35:23 -0300

    Removido codigos desnecessarios

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,20 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./Components/Header/Header";
-import MenuBar from "./Components/MenuBar/MenuBar";
 import { GlobalStyle, ContainerPages, MainMiddle } from "./styles/global";
 import Home from "./Pages/Home/Home";
-import api from "./api/api";
-import { useEffect, useState } from "react";
-import CardsRecents from "./Components/Cards/CardsRecents/CardsRecents";
-import InfosCards from "./Components/Cards/InfosCards/InfosCards";
 import Games from "./Pages/Games/Games";
 import GamesProvider from "./context/GamesContext";
 import Footer from "./Components/Footer/Footer";
 
 function App() {
+
+
   return (
     <BrowserRouter>
       <GamesProvider>
 
         <Header />
         <ContainerPages>
-          {/*  <MenuBar /> */}
           <MainMiddle>
             <Routes>
               <Route path="/" element={<Home />}></Route>
