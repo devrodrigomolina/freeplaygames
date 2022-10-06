b17225174c2a25cedb96575785cdc72d567b586d
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2022-10-06 00:34:48 -0300

    Começo da implantação de pesquisa por jogo especifico

--- src/Components/Input/Input.jsx
+++ src/Components/Input/Input.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { InputContainer } from './style'
 import { BiSearch } from 'react-icons/bi'
 
 const Input = () => {
+  const [search, setSearch] = useState('')
+
   return (
     <InputContainer>
       <BiSearch className='icon' />
-      <input type='text' placeholder='Digite algo para pesquisar'/>
+      <input type='text' value={search} onChange={({ target }) => setSearch(target.value)} placeholder='Digite algo para pesquisar'/>
     </InputContainer>
   )
 }
