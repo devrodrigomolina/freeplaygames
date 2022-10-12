9ded366c6d191bc7ecfbbaeded8219163ca1665d
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2022-10-12 19:50:28 -0300

    Alterado posicionamento

--- src/Pages/Home/style.jsx
+++ src/Pages/Home/style.jsx
@@ -2,10 +2,11 @@ import styled from "styled-components";
 import banner from '../../assets/hero-1-1.jpg'
 
 export const HomeContainer = styled.div`
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: auto;
   background-image: url(${banner});
   background-size: cover;
+  background-position: center;
   background-repeat: no-repeat;
   position: relative;
   z-index: 3;
@@ -17,8 +18,8 @@ export const Amarelo = styled.div`
   height: 90vh;
   position: relative;
   z-index: 1;
-  top: -90px;
-  left: -300px;
+  top: 76%;
+  left: -16%;
   -webkit-clip-path: polygon(31% -44%,95% 25%,100% calc(100% - 250px),101% 113%,15% calc(78% - 230px));
   clip-path: polygon(31% -44%,95% 25%,100% calc(100% - 250px),101% 113%,15% calc(78% - 230px));
   
