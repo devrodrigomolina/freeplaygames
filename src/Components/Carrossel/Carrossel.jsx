870820253d535b94d50ab56d24385d6b7d58703b
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2022-10-12 19:52:01 -0300

    Update Carrossel.jsx

--- src/Components/Carrossel/Carrossel.jsx
+++ src/Components/Carrossel/Carrossel.jsx
@@ -14,22 +14,7 @@ const Carrossel = () => {
 
   return (
     <CarrosselContainer>
-      <h1 style={{width: '850px',margin: '190px', color: 'white', fontSize: '7rem', letterSpacing: '10px'}}>WELCOME TO <span style={{color: '#FEBF10'}}>FREEPLAY</span> GAMING ZONE.</h1>
-{/*       <Swiper
-        cssMode={true}
-        navigation={true}
-        pagination={true}
-        mousewheel={true}
-        keyboard={true}
-        modules={[Navigation, Pagination, Mousewheel, Keyboard]}
-        className="mySwiper"
-      >
-        {limitData.map(games =>
-          <SwiperSlide>
-            <img src={games.thumbnail}/>
-          </SwiperSlide>
-        )}
-      </Swiper> */}
+{/*       <h1 style={{width: '850px',margin: '190px', color: 'white', fontSize: '7rem', letterSpacing: '10px'}}>WELCOME TO <span style={{color: '#FEBF10'}}>FREEPLAY</span> GAMING ZONE.</h1> */}
     </CarrosselContainer>
   )
 }
