21e35b02b0c84ff223264b8f4e179d3630fb7004
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2022-10-12 19:50:00 -0300

    Criado pagina de lançamentos

---
+++ src/Pages/Releases/Releases.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import CardsReleases from '../../Components/Cards/CardsReleases/CardsReleases'
+
+import { ContainerReleases } from './styles'
+
+const Releases = () => {
+  return (
+    <ContainerReleases>
+      <CardsReleases />
+    </ContainerReleases>
+  )
+}
+
+export default Releases
